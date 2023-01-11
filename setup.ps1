103a3649c46a0b445ae4167619d817d76d420ee8
sagiavisar <47398873+sagiavisar@users.noreply.github.com>
2023-01-11 09:34:35 +0000

    Adding setup script and PowerShell profile

---
+++ setup.ps1
@@ -0,0 +1,15 @@
+# PSReadLine is a module that provides additional functionality to the PowerShell console, such as syntax highlighting and command history.
+# It also provides a number of key bindings that are similar to those used in Bash, which can help to make the transition between the two shells more seamless.
+# To install PSReadLine, you can use the PowerShell command:
+Install-Module -Name PSReadLine
+
+# You also can check other PowerShell profile like "posh-git".
+# "posh-git" that bring git integration into PowerShell, that enables you to see
+    # the current branch,
+    # the state of files in the working directory, 
+    # and much more information all in the console.
+# The installation process is:
+Install-Module posh-git
+
+# Terminal Icons Install
+Install-Module -Name Terminal-Icons -Repository PSGallery
